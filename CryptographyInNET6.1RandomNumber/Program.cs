6c8dd7a31b513f4dfe4654acdda8284d2143d0ef
T <tomaszbanach12@gmail.com>
2022-07-11 21:57:31 +0200

    cyptographic random numbers

--- CryptographyInNET6.1RandomNumber/Program.cs
+++ CryptographyInNET6.1RandomNumber/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Security.Cryptography;
+
+Console.WriteLine("Random number demonstration in .NET");
+Console.WriteLine("-----------------------------------");
+Console.WriteLine();
+
+for (int i = 0; i < 10; i++)
+{
+    var randomNumbers = RandomNumberGenerator.GetBytes(32);
+    Console.WriteLine($"Random number {i}: {Convert.ToBase64String(randomNumbers)}");
+}
+
+Console.WriteLine();
